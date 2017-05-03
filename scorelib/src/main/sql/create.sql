7e753f581f4dde1430de76382ee1947303c25fc5
Pasi Ahopelto <pahopelt@gmail.com>
2017-05-03 19:09:23 +0300

    scorelib: add indices to DB tables
    Some of the fields were missing indices and constrains
    as per current understanding of the model. Updated.

--- scorelib/src/main/sql/create.sql
+++ scorelib/src/main/sql/create.sql
@@ -1,12 +1,12 @@
 create table party (
 	id integer,
-	name varchar(64),
+	name varchar(64) unique not null,
 	primary key (id)
 );
 
 create table candidate (
 	id integer,
-	name varchar(64),
+	name varchar(64) unique not null,
 	current_party_id integer,
 	primary key (id),
 	foreign key (current_party_id) references party(id) on delete cascade
@@ -14,27 +14,28 @@ create table candidate (
 
 create table voting (
 	id integer,
-	official_name varchar(64),
-	description varchar(64),
-	scoring enum ('exact', 'distance'),
+	name varchar(64) unique not null,
+	description varchar(256) not null,
+	scoring enum ('exact', 'distance') not null,
 	primary key (id)
 );
 
 create table voting_option (
 	id integer,
-	voting_id integer,
-	name varchar(64),
-	position integer,
+	voting_id integer not null,
+	name varchar(64) not null,
+	position integer not null,
 	primary key (id),
 	foreign key (voting_id) references voting(id) on delete cascade,
+	unique key voting_option_idx (voting_id, name),
 	constraint unique (voting_id, name)
 );
 
 create table vote (
 	id integer,
-	voting_id integer,
-	candidate_id integer,
-	option_id integer,
+	voting_id integer not null,
+	candidate_id integer not null,
+	option_id integer not null,
 	primary key (id),
 	foreign key (voting_id) references voting(id) on delete cascade,
 	foreign key (candidate_id) references candidate(id) on delete cascade,
