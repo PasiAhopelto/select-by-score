e4e90f43fafeeeea893fa81219048a1beb44950d
Pasi Ahopelto <pahopelt@gmail.com>
2017-05-08 20:23:35 +0300

    scorelib: fix dependencies
    Replaced opencsv with commons-lang, because csv reading
    isn't needed, but EqualsBuilder, etc are.

---
+++ scorelib/src/test/java/github/pasiahopelto/scorelib/reader/TestOptionParser.java
@@ -0,0 +1,10 @@
+package github.pasiahopelto.scorelib.reader;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestOptionParser {
+
+	private OptionParser parser = new OptionParser();
+}
