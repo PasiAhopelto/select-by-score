891cadfdae4855b999c6ef9ad7a1241e90fbf12e
Pasi Ahopelto <pahopelt@gmail.com>
2017-05-17 19:44:28 +0300

    scorelib: parse voting
    The parser parses lines starting with voting and add
    lines starting with option into the previously read
    voting.

---
+++ scorelib/src/main/java/github/pasiahopelto/scorelib/reader/VotingParser.java
@@ -0,0 +1,51 @@
+package github.pasiahopelto.scorelib.reader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+import github.pasiahopelto.scorelib.model.Voting;
+
+public class VotingParser implements LineVisitor {
+
+	private ArrayList<Voting> votings = Lists.newArrayList();
+
+	public void parseEntity(ParsedLine parsedLine) throws ParseException {
+		if("voting".equals(parsedLine.getType())) {
+			addVoting(parsedLine);
+		}
+		if("option".equals(parsedLine.getType())) {
+			addOptionToVoting(parsedLine);
+		}
+	}
+
+	private void addVoting(ParsedLine parsedLine) throws ParseException {
+		if(parsedLine.getValues().size() != 2) {
+			throw new ParseException("expected name and description of voting and not " + parsedLine.getValues());
+		}
+		Voting voting = new Voting();
+		voting.setName(parsedLine.getValues().get(0));
+		voting.setDescription(parsedLine.getValues().get(1));
+		votings.add(voting);
+	}
+
+	private void addOptionToVoting(ParsedLine parsedLine) throws ParseException {
+		if(votings.isEmpty()) {
+			throw new ParseException("option should have preceding voting at " + parsedLine.getValues());
+		}
+		if(parsedLine.getValues().size() != 1) {
+			throw new ParseException("expedted option value, not " + parsedLine.getValues());
+		}
+		Voting currentVoting = votings.get(votings.size() - 1);
+		if(currentVoting.getOptions() == null) {
+			currentVoting.setOptions(new ArrayList<String>());
+		}
+		String option = parsedLine.getValues().get(0);
+		currentVoting.getOptions().add(option);
+	}
+
+	public List<Voting> getVotings() {
+		return votings;
+	}
+}
---
+++ scorelib/src/test/java/github/pasiahopelto/scorelib/reader/TestVotingParser.java
@@ -0,0 +1,82 @@
+package github.pasiahopelto.scorelib.reader;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.google.common.collect.Lists;
+
+import github.pasiahopelto.scorelib.model.Voting;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestVotingParser {
+
+	private static final String DESCRIPTION = "description";
+	private static final String NAME = "name";
+	private static final String OPTION = "option";
+	
+	private final ParsedLine nonVotingLine = new ParsedLine();
+	private final ParsedLine votingLine = new ParsedLine();
+	private final ParsedLine optionLine = new ParsedLine();
+	{
+		nonVotingLine.setType("party");
+		votingLine.setType("voting");
+		votingLine.setValues(Lists.newArrayList(NAME, DESCRIPTION));
+		optionLine.setType("option");
+		optionLine.setValues(Lists.newArrayList(OPTION));
+	}
+	
+	private VotingParser parser = new VotingParser();
+	
+	@Test
+	public void isLineVisitor() {
+		assertTrue(parser instanceof LineVisitor);
+	}
+	
+	@Test
+	public void ignoresNonVotingLine() throws ParseException {
+		parser.parseEntity(nonVotingLine);
+		assertTrue(parser.getVotings().isEmpty());
+	}
+	
+	@Test(expected=ParseException.class)
+	public void throwsExceptionWhenNameAndDescriptionAreMissing() throws ParseException {
+		votingLine.getValues().clear();
+		parser.parseEntity(votingLine);
+		assertTrue(parser.getVotings().isEmpty());
+	}
+	
+	@Test
+	public void parsesVoting() throws ParseException {
+		parser.parseEntity(votingLine);
+		assertEquals(1, parser.getVotings().size());
+		Voting voting = parser.getVotings().get(0);
+		assertEquals(NAME, voting.getName());
+		assertEquals(DESCRIPTION, voting.getDescription());
+	}
+	
+	@Test(expected=ParseException.class)
+	public void throwsExceptionIfOptionWithoutVoting() throws ParseException {
+		parser.parseEntity(optionLine);
+	}
+
+	@Test(expected=ParseException.class)
+	public void throwsExceptionWhenOptionValueIsMissing() throws ParseException {
+		optionLine.getValues().clear();
+		parser.parseEntity(votingLine);
+		parser.parseEntity(optionLine);
+	}
+
+	@Test
+	public void addsOptionToVoting() throws ParseException {
+		parser.parseEntity(votingLine);
+		parser.parseEntity(optionLine);
+		List<String> options = parser.getVotings().get(0).getOptions();
+		assertEquals(1, options.size());
+		assertEquals(OPTION, options.get(0));
+	}
+}
