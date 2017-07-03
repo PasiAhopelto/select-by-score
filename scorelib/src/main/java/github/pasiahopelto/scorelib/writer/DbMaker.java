8bcef86a31d7089e53c26655f8516bafa7e3c421
Pasi Ahopelto <pahopelt@gmail.com>
2017-07-03 21:15:17 +0300

    scorelib: create tables
    Db maker creates DB tables and returns a data source bean.

---
+++ scorelib/src/main/java/github/pasiahopelto/scorelib/writer/DbMaker.java
@@ -0,0 +1,29 @@
+package github.pasiahopelto.scorelib.writer;
+
+import javax.sql.DataSource;
+
+import org.h2.jdbcx.JdbcConnectionPool;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+public class DbMaker {
+
+	private static final String CREATE_SQL = "create.sql";
+
+	@Bean(name = "dataSource")
+	public DataSource createDatabase() {
+		DataSource dataSource = JdbcConnectionPool.create("jdbc:h2:mem:test", "sa", "");
+        DatabasePopulatorUtils.execute(createDatabasePopulator(), dataSource);
+		return dataSource;
+	}
+
+	private DatabasePopulator createDatabasePopulator() {
+        ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+        databasePopulator.setContinueOnError(true);
+        databasePopulator.addScript(new ClassPathResource(CREATE_SQL));
+        return databasePopulator;
+    }
+}
---
+++ scorelib/src/test/java/github/pasiahopelto/scorelib/writer/TestDbMaker.java
@@ -0,0 +1,62 @@
+package github.pasiahopelto.scorelib.writer;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestDbMaker {
+
+	@InjectMocks
+	private DbMaker dbMaker;
+
+	@Test
+	public void returnsDataSourceThatHasConnection() throws SQLException {
+		DataSource createDatabase = dbMaker.createDatabase();
+		Connection connection = createDatabase.getConnection();
+		connection.close();
+	}
+
+	@Test
+	public void createsPartyTable() throws SQLException {
+		verifyTableExists("party");
+	}
+
+	@Test
+	public void createsCandidateTable() throws SQLException {
+		verifyTableExists("candidate");
+	}
+
+	@Test
+	public void createsVotingTable() throws SQLException {
+		verifyTableExists("voting");
+	}
+
+	@Test
+	public void createsVoteTable() throws SQLException {
+		verifyTableExists("vote");
+	}
+
+	@Test
+	public void createsVotingOptionTable() throws SQLException {
+		verifyTableExists("voting_option");
+	}
+
+	private void verifyTableExists(String table) {
+		DataSource dataSource = dbMaker.createDatabase();
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		SqlRowSet sqlRowSet = jdbcTemplate.queryForRowSet("select count(*) from " + table);
+		assertTrue(sqlRowSet.first());
+		assertEquals(0, sqlRowSet.getInt(1));
+	}
+}
