4406b07574275b5a9a1c3fca90b9928b0dd18dd1
Pasi Ahopelto <pahopelt@gmail.com>
2018-01-11 21:50:00 +0200

    scorelib: package spring boot application
    Uses spring boot plugin to create a runnable jar with all dependencies
    with "mvn package". Currently runs with "java -jar <jar>". This is a
    first step towards self packaging app with startup script.

---
+++ scorelib/src/main/java/github/pasiahopelto/scorelib/run/Main.java
@@ -0,0 +1,12 @@
+package github.pasiahopelto.scorelib.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApplication.class, args);
+	}
+}
