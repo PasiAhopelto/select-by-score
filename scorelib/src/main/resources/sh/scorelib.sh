96f0527fd24c1201c2e31132b54d453709994b01
Pasi Ahopelto <pahopelt@gmail.com>
2018-01-31 20:12:00 +0200

    scorelib: write pid to var/scorelib.pid
    The PID can be used when killing scorelib instance forcibly.

--- scorelib/src/main/resources/sh/scorelib.sh
+++ scorelib/src/main/resources/sh/scorelib.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 LIBDIR=`dirname $0`/../lib
+VARDIR=`dirname $0`/../var
 JAR=$LIBDIR/scorelib-1.0-SNAPSHOT.jar
 
 if [ "$1" = "start" ]; then
 	java -jar $JAR &
+	echo $! > $VARDIR/scorelib.pid
 	disown
 elif [ "$1" = "stop" ]; then
 	1
