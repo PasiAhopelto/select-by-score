8f20dda2b55d89c6def1eadb713d1c4c2f23b896
Pasi Ahopelto <pahopelt@gmail.com>
2018-02-12 21:40:56 +0200

    scorelib: start and stop scorelib
    Stop calls scorelib's shutdown rest api, and start waits (with curl)
    until health api reports that the service is running. Acceptance test
    for start and stop passes when scorelib is built with "mvn package".

--- scorelib/src/main/resources/sh/scorelib.sh
+++ scorelib/src/main/resources/sh/scorelib.sh
@@ -5,11 +5,13 @@ VARDIR=`dirname $0`/../var
 JAR=$LIBDIR/scorelib-1.0-SNAPSHOT.jar
 
 if [ "$1" = "start" ]; then
-	java -jar $JAR &
+	java -Dendpoints.shutdown.enabled=true -Dendpoints.shutdown.sensitive=false -jar $JAR &
 	echo $! > $VARDIR/scorelib.pid
 	disown
+	curl --retry 15 --retry-delay 2 --retry-connrefused -s --show-error http://localhost:8080/health
 elif [ "$1" = "stop" ]; then
-	1
+	curl -X POST http://localhost:8080/shutdown
+	rm -f $VARDIR/scorelib.pid
 else 
 	echo "usage: $0 [start|stop]"
 fi
