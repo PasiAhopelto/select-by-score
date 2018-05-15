58f7d0de6c9187adf504422b3cc43db88e2f9af5
Pasi Ahopelto <pahopelt@gmail.com>
2018-05-15 21:49:35 +0300

    use spring boot 2 and spring 5

--- scorelib/src/main/resources/sh/scorelib.sh
+++ scorelib/src/main/resources/sh/scorelib.sh
@@ -9,7 +9,7 @@ if [ "$1" = "start" ]; then
 	java -Dendpoints.shutdown.enabled=true -Dendpoints.shutdown.sensitive=false -jar $JAR -classpath=$DATADIR &
 	echo $! > $VARDIR/scorelib.pid
 	disown
-	curl --retry 15 --retry-delay 2 --retry-connrefused -s --show-error http://localhost:8080/health
+	curl --retry 15 --retry-delay 2 --retry-connrefused -s --show-error http://localhost:8080/actuator/health
 elif [ "$1" = "stop" ]; then
 	curl -X POST http://localhost:8080/shutdown
 	rm -f $VARDIR/scorelib.pid
